104b324a30c1c5d936fd2cd5d168f2b374a6efba
beanteacher <dh65432@naver.com>
2025-03-14 10:12:37 +0900

    feat: first commit

---
+++ src/main/java/ringle/first/assignment/Ringle1stAssignmentApplication.java
@@ -0,0 +1,13 @@
+package ringle.first.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ringle1stAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ringle1stAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ringle/first/assignment/Ringle1stAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ringle.first.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ringle1stAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
